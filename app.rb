b5621d3474f4b19c57a648159030971cc6ab837c
Yefim Vedernikoff <yefim323@gmail.com>
2017-11-19 21:20:18 -0800

    Add better instructions

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 
 get '/' do
   'This is a simple service/website/whatever for you to use as you desire. '\
-  'Try it out by simply adding "5" to the URL after the trailing slash. '\
-  'With a /5 it should take ~5 seconds to load the site. '\
+  'Try it out by simply adding "/5" to the URL. '\
+  'With a /5 it should take ~5 seconds to load the site. '
 end
 
 get '/:n' do |n|

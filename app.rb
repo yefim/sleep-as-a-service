3be866f82a1463b21242b1d78d8ee19a28f75fd9
Yefim Vedernikoff <yefim323@gmail.com>
2017-11-19 20:49:04 -0800

    Back to simpler times

--- app.rb
+++ app.rb
@@ -1,15 +1,4 @@
 require 'sinatra'
-require 'sinatra/cross_origin'
-
-configure do
-  enable :cross_origin
-end
-
-options '*' do
-  response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
-  response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
-  200
-end
 
 get '/:n' do |n|
   sleep n.to_i

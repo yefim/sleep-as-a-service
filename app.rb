d5280661cf9252f6a5ae892e176e656e170818b5
Yefim Vedernikoff <yefim323@gmail.com>
2017-11-19 20:43:19 -0800

    Add CORS support

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
+
+options '*' do
+  response.headers['Allow'] = 'HEAD,GET,PUT,POST,DELETE,OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
+  200
+end
+
 get '/:n' do |n|
   sleep n.to_i
   n

98a29ea7bfea99bd9b69e4988f01278c87595574
Yefim Vedernikoff <yefim323@gmail.com>
2017-11-19 21:18:46 -0800

    Add a huide

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+get '/' do
+  'This is a simple service/website/whatever for you to use as you desire. '\
+  'Try it out by simply adding "5" to the URL after the trailing slash. '\
+  'With a /5 it should take ~5 seconds to load the site. '\
+end
+
 get '/:n' do |n|
   sleep n.to_i
   headers 'Access-Control-Allow-Origin' => '*'
